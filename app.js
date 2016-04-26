0d5a85163086ada607b0434acb12cd85f82c9e95
aaVIaDoR io <vid.repar@gmail.com>
2016-04-26 08:52:31 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$("#left-scroll").click(function(){
+    $("#carousel-ul").animate({left: '-=100%'});
+});
+$("#right-scroll").click(function(){
+    $("#carousel-ul").animate({left: '+=100%'});
+});
